004dda70cd8cdd046d7c3f85f1ef6d81a263ffff
Santi Puch <santipuch590@gmail.com>
2017-11-09 19:38:02 +0100

    Low level segmentation implementation

--- PGM_exercises/low_level_segmentation/UGM_segmentation.m
+++ PGM_exercises/low_level_segmentation/UGM_segmentation.m
@@ -2,21 +2,21 @@ clear all;
 close all;
 clc;
 
-im_name='3_12_s.bmp';
+im_name='7_9_s.bmp';
 
 % TODO: Update library path
 % Add  library paths
-basedir='~/Desenvolupament/UGM/';
-addpath(basedir);
+% basedir='~/Desenvolupament/UGM/';
+% addpath(basedir);
 
 
 
 %Set model parameters
 %cluster color
-K=4; % Number of color clusters (=number of states of hidden variables)
+K=3; % Number of color clusters (=number of states of hidden variables)
 
 %Pair-wise parameters
-smooth_term=[0.0 2]; % Potts Model
+smooth_term=[0.0 10]; % Potts Model
 
 %Load images
 im = imread(im_name);
@@ -28,19 +28,20 @@ NumCols = size(im,2);
 %Convert to LAB colors space
 % TODO: Uncomment if you want to work in the LAB space
 %
-% im = RGB2Lab(im);
-
-
+im = RGB2Lab(im);
 
 %Preparing data for GMM fiting
 %
 % TODO: define the unary energy term: data_term
 % nodePot = P( color at pixel 'x' | Cluster color 'c' )  
 
+im=double(im);
+x=reshape(im, [size(im,1)*size(im,2) size(im,3)]);
+gmm_color = gmdistribution.fit(x,K);
+mu_color = gmm_color.mu;
 
-nodePot=[];
-
-
+data_term=gmm_color.posterior(x);
+nodePot = data_term;
 
 %Building 4-grid
 %Build UGM Model for 4-connected segmentation
@@ -57,9 +58,9 @@ if ~isempty(edgePot)
     im_c= reshape(mu_color(c,:),size(im));
     
     % Call different UGM inference algorithms
-    display('Loopy Belief Propagation'); tic;
-    [nodeBelLBP,edgeBelLBP,logZLBP] = UGM_Infer_LBP(nodePot,edgePot,edgeStruct);toc;
-    im_lbp = max(nodeBelLBP,[],2);
+%     display('Loopy Belief Propagation'); tic;
+%     [nodeBelLBP,edgeBelLBP,logZLBP] = UGM_Infer_LBP(nodePot,edgePot,edgeStruct);toc;
+%     im_lbp = max(nodeBelLBP,[],2);
     
     % Max-sum
     display('Max-sum'); tic;
@@ -71,13 +72,24 @@ if ~isempty(edgePot)
     % TODO: apply other inference algorithms and compare their performance
     %
     % - Graph Cut
+%     display('Graph cut'); tic;
+%     decodeGcut = UGM_Decode_GraphCut(nodePot,edgePot,edgeStruct);
+%     im_gcut = reshape(mu_color(decodeGcut,:), size(im));
+%     toc;
+
     % - Linear Programing Relaxation
+%     display('Linear Programming'); tic;
+%     decodeLinProg = UGM_Decode_LinProg(nodePot,edgePot,edgeStruct);
+%     im_linprog = reshape(mu_color(decodeLinProg, :), size(im));
+%     toc;
+    
     
     figure
     subplot(2,2,1),imshow(Lab2RGB(im));xlabel('Original');
     subplot(2,2,2),imshow(Lab2RGB(im_c),[]);xlabel('Clustering without GM');
     subplot(2,2,3),imshow(Lab2RGB(im_bp),[]);xlabel('Max-Sum');
-    subplot(2,2,4),imshow(Lab2RGB(im_lbp),[]);xlabel('Loopy Belief Propagation');
+%     subplot(2,2,4),imshow(Lab2RGB(im_lbp),[]);xlabel('Loopy Belief Propagation');
+%     subplot(2,2,4),imshow(Lab2RGB(im_linprog),[]);xlabel('Linear Programming');
     
 else
    
